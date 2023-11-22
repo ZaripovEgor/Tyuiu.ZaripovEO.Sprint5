53578b8159ac9b1bfaa75fc93b5e2de3d556bae0
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-11-22 19:54:04 +0500

    Task4

---
+++ Tyuiu.ZaripovEO.Sprint5.Task4.V30.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZaripovEO.Sprint5.Task4.V30.Lib
+{
+    public class DataService : ISprint5Task4V30
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round(Convert.ToDouble(strX), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZaripovEO.Sprint5.Task4.V30/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZaripovEO.Sprint5.Task4.V30.Lib;
+using System.IO;
+
+namespace Tyuiu.ZaripovEO.Sprint5.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №5 | Выполнил: Зарипов Е. О. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Зарипов Егор Олегович | ИСТНб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти максимальное целое число в файле, которое является простым числом.*");
+            Console.WriteLine("* Полученный результат вывести на консоль. У вещественных значений        *");
+            Console.WriteLine("* округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V30.txt";
+
+            Console.WriteLine("Данные находятся в файле" + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
