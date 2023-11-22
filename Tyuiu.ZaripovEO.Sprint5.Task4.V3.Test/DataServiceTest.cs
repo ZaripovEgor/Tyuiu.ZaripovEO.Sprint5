f5e77b0b475be2d31c8a523343606b206ee6d7c4
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-11-22 20:51:22 +0500

    Task4

---
+++ Tyuiu.ZaripovEO.Sprint5.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.ZaripovEO.Sprint5.Task4.V3.Lib;
+
+namespace Tyuiu.ZaripovEO.Sprint5.Task4.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\zarip\source\repos\Tyuiu.ZaripovEO.Sprint5\Tyuiu.ZaripovEO.Sprint5.Task4.V3\bin\Debug\InPutDataFileTask4V3.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
--- Tyuiu.ZaripovEO.Sprint5.Task4.V3/Program.cs
+++ Tyuiu.ZaripovEO.Sprint5.Task4.V3/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.ZaripovEO.Sprint5.Task4.V3.Lib;
+using System.IO;
 
 namespace Tyuiu.ZaripovEO.Sprint5.Task4.V3
 {
@@ -10,6 +12,33 @@ namespace Tyuiu.ZaripovEO.Sprint5.Task4.V3
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+            Console.Title = "Спринт №5 | Выполнил: Зарипов Е. О. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Зарипов Егор Олегович | ИСТНб-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+            Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль.               *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V3.txt";
+            Console.WriteLine("Данные нахлжятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
         }
     }
 }
